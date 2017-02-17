7ce54bc32a89f2dc651e73ba97f0a73af0fa6458
Babynorth <791151858@qq.com>
2017-02-17 11:01:44 +0800

    test1

--- bis-api/src/main/java/com/babynorth/entity/Test.java
+++ bis-api/src/main/java/com/babynorth/entity/Test.java
@@ -6,6 +6,6 @@ package com.babynorth.entity;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("tt1如意大傻逼哦！");
+        System.out.println("tt1如意大傻逼哦1！");
     }
 }
