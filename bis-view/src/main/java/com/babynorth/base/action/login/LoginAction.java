466e5489e6a3a0bfc34d603942de410ddbec0680
Babynorth <791151858@qq.com>
2017-03-03 09:46:42 +0800

    test1

---
+++ bis-view/src/main/java/com/babynorth/base/action/login/LoginAction.java
@@ -0,0 +1,11 @@
+package com.babynorth.base.action.login;
+
+/**
+ * Created by Administrator on 2017/3/2.
+ */
+public class LoginAction {
+
+    public String main() {
+        return "login";
+    }
+}
